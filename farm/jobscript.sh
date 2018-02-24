e658b03e9678d76f6bc2c003122c55e7f4d262b6
Taylor Elaine Reiter <tereiter@farm.cse.ucdavis.edu>
2018-02-23 16:13:51 -0800

    update farm submit scripts

--- farm/jobscript.sh
+++ farm/jobscript.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-module use /opt/software/ged-software/modulefiles/
-module load anaconda
-module unload python
-module swap GNU GNU/6.2
-source activate 2017-paper-gather
+source modules.farm
 
 {exec_job}
